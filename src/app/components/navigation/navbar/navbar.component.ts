ecd37be2aafcefec097c6ee3da8f650df0927f43
Jan G. Wieners <jan@jan-wieners.de>
2019-07-30 23:45:25 +0200

    Cleanup.

--- src/app/components/navigation/navbar/navbar.component.ts
+++ src/app/components/navigation/navbar/navbar.component.ts
@@ -1,35 +1,33 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
-
 import {TranslateService} from '@ngx-translate/core';
+
 import {AccountService} from '../../../services/account.service';
 import {AuthDialogComponent} from '../../auth-dialog/auth-dialog.component';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
 
   @Output() public sidenavToggle = new EventEmitter();
 
-  public isAuthenticated = false;
+  public isAuthenticated;
   public languages;
 
   constructor(
     private account: AccountService,
     private dialog: MatDialog,
     public translate: TranslateService) {
+
+    this.isAuthenticated = false;
     this.languages = this.translate.getLangs();
     this.account.isUserAuthenticatedObservable.subscribe(state => this.isAuthenticated = state);
   }
 
   ngOnInit() {
-  }
-
-  public setLanguage(language) {
-    this.translate.setDefaultLang(language);
   }
 
   public onToggleSidenav() {
