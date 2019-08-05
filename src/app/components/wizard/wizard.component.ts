923a413652b7ad9aaf2b97da2f53a4fd927e9d53
Kai Niebes <kai.niebes@outlook.com>
2019-08-05 14:02:22 +0200

    WizardComponent: Use ContentProviderService for JSON entity parsing

--- src/app/components/wizard/wizard.component.ts
+++ src/app/components/wizard/wizard.component.ts
@@ -19,6 +19,7 @@ import {
   basePlace,
 } from '../metadata/base-objects';
 import { MongoHandlerService } from '../../services/mongo-handler.service';
+import { ContentProviderService } from '../../services/content-provider.service';
 import { IEntity, IFile } from '../../interfaces';
 import { mock } from '../../../assets/mock';
 
@@ -40,8 +41,10 @@ export class WizardComponent implements AfterViewInit {
   @ViewChild('stepPublish', { static: false })
   stepPublish: MatStep | undefined;
 
-  public UploadResult: any | undefined = JSON.parse(mock.upload);
-  public SettingsResult: any | undefined = JSON.parse(mock.settings);
+  // public UploadResult: any | undefined = JSON.parse(mock.upload);
+  public UploadResult: any | undefined;
+  // public SettingsResult: any | undefined = JSON.parse(mock.settings);
+  public SettingsResult: any | undefined;
 
   // The entity gets validated inside of the metadata/entity component
   // but we also keep track of validation inside of the wizard
@@ -64,6 +67,7 @@ export class WizardComponent implements AfterViewInit {
     private uuid: UuidService,
     private mongo: MongoHandlerService,
     private router: Router,
+    private content: ContentProviderService,
   ) {
     window.onmessage = async message => {
       const type = message.data.type;
@@ -91,8 +95,6 @@ export class WizardComponent implements AfterViewInit {
           console.log(message.data);
       }
     };
-
-    this.JSONEntityToValidationEntity();
 
     this.uploadHandler.$UploadResult.subscribe(
       result => (this.UploadResult = result),
@@ -169,164 +171,16 @@ export class WizardComponent implements AfterViewInit {
   }
 
   public validationEntityToJSONEntity() {
-    const walk = (obj: any) => {
-      const resultObj: any = {};
-      if (typeof obj === 'string') {
-        return obj;
-      }
-
-      for (const prop in obj) {
-        if (!obj.hasOwnProperty(prop)) {
-          continue;
-        }
-        const current = obj[prop];
-        const value = obj[prop].value;
-        const isArray = Array.isArray(value);
-        const isString = typeof value === 'string';
-
-        if (!isArray && isString) {
-          resultObj[prop] = value;
-        } else if (isArray && !isString) {
-          resultObj[prop] = (value as any[]).map(walk);
-        } else {
-          switch (prop) {
-            case 'address':
-            case 'place':
-              resultObj[prop] = walk(value);
-              break;
-            default:
-              console.log(
-                'Unkown hit in conversion',
-                current,
-                prop,
-                obj,
-                obj[prop],
-              );
-          }
-        }
-      }
-      return resultObj;
-    };
-
-    const resultEntity = walk(this.entity);
+    const resultEntity = this.content.convertValidationEntityToJSON(
+      this.entity,
+    );
     console.log('Validation entity converted to JSON:', resultEntity);
     return resultEntity;
   }
 
   public JSONEntityToValidationEntity() {
     const _parsed = JSON.parse(mock.json);
-
-    // Helper function to fill an array with length of 'length'
-    // with the resulting base validation object of 'func'
-    const arrBase = (func: () => any, valArr: any[]) => {
-      const arr = new Array(valArr.length);
-      for (let i = 0; i < valArr.length; i++) {
-        arr[i] = walkSimple(valArr[i], func);
-      }
-      return arr;
-    };
-
-    // Walk simple object types, e.g. person, institution, link, id...
-    const walkSimple = (parsed: any, func: () => any) => {
-      const newSimple = { ...func() };
-      for (const prop in parsed) {
-        if (!parsed.hasOwnProperty(prop)) {
-          continue;
-        }
-
-        const value = parsed[prop];
-        const isArray = Array.isArray(value);
-        const isString = typeof value === 'string';
-        if (!isArray && isString) {
-          newSimple[prop].value = value;
-        } else if (isArray && !isString) {
-          switch (prop) {
-            case 'institution':
-              newSimple[prop].value = arrBase(baseInstitution, value);
-              break;
-            default:
-              // Assume string array
-              newSimple[prop].value = value;
-          }
-        } else {
-          switch (prop) {
-            case 'address':
-              newSimple[prop].value = walkSimple(value, baseAddress);
-              break;
-            case 'place':
-            default:
-              console.log('Unknown hit in simple', prop, value, parsed);
-          }
-        }
-      }
-      return newSimple;
-    };
-
-    // Walk digital or physical entities, which are the 2 top level types
-    const walkEntity = (parsed: any, isPhysical = false) => {
-      // Base to be overwritten
-      const newBase = isPhysical
-        ? { ...baseEntity(), ...basePhysical() }
-        : { ...baseEntity(), ...baseDigital() };
-
-      for (const prop in parsed) {
-        if (!parsed.hasOwnProperty(prop)) {
-          continue;
-        }
-
-        const value = parsed[prop];
-        const isArray = Array.isArray(value);
-        const isString = typeof value === 'string';
-        if (!isArray && isString) {
-          newBase[prop].value = value;
-        } else if (isArray && !isString) {
-          const valArr: any[] = value as any[];
-          const len = valArr.length;
-          switch (prop) {
-            case 'externalId':
-              newBase[prop].value = arrBase(baseExternalId, valArr);
-              break;
-            case 'externalLink':
-              newBase[prop].value = arrBase(baseExternalLink, valArr);
-              break;
-            case 'persons':
-              newBase[prop].value = arrBase(basePerson, valArr);
-              break;
-            case 'institutions':
-              newBase[prop].value = arrBase(baseInstitution, valArr);
-              break;
-            case 'dimensions':
-              newBase[prop].value = arrBase(baseDimension, valArr);
-              break;
-            case 'creation':
-              newBase[prop].value = arrBase(baseCreation, valArr);
-              break;
-            case 'phyObjs':
-              newBase[prop].value = new Array(len);
-              for (let i = 0; i < len; i++) {
-                newBase[prop].value[i] = walkEntity(valArr[i], true);
-              }
-              break;
-            default:
-              newBase[prop].value = value;
-          }
-        } else {
-          switch (prop) {
-            case 'place':
-              newBase[prop] = {
-                required: false,
-                value: walkSimple(value, basePlace),
-              };
-              break;
-            default:
-              console.log('Unknown hit in parsing', prop, value);
-          }
-        }
-      }
-      return newBase;
-    };
-
-    const _base = walkEntity(_parsed);
+    const _base = this.content.walkEntity(_parsed);
     console.log('JSON entity parsed to validation Entity:', _base);
     this.entity = (_base as unknown) as any;
     this.validateEntity();
--- src/app/services/content-provider.service.ts
+++ src/app/services/content-provider.service.ts
@@ -1,20 +1,213 @@
 import { Injectable } from '@angular/core';
-import { ReplaySubject } from 'rxjs';
 
 import { MongoHandlerService } from './mongo-handler.service';
-import { IEntity } from '../interfaces';
+import {
+  IEntity,
+  IMetaDataPerson,
+  IMetaDataInstitution,
+  IMetaDataTag,
+} from '../interfaces';
+import {
+  baseAddress,
+  baseExternalId,
+  baseExternalLink,
+  baseDimension,
+  baseCreation,
+  baseInstitution,
+  basePerson,
+  baseEntity,
+  baseDigital,
+  basePhysical,
+  basePlace,
+} from '../components/metadata/base-objects';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ContentProviderService {
-  private EntitiesSubject = new ReplaySubject<IEntity[]>();
-  public EntitiesObservable = this.EntitiesSubject.asObservable();
+  // Existing Server data
+  private ServerPersons: IMetaDataPerson[] = [];
+  private ServerInstitutions: IMetaDataInstitution[] = [];
+  private ServerTags: IMetaDataTag[] = [];
 
   constructor(private mongo: MongoHandlerService) {
+    // TODO: refetch on some occasions, e.g. after wizard completion
     this.mongo
-      .getAllEntities()
-      .then(result => this.EntitiesSubject.next(result))
-      .catch(err => console.error(err));
+      .getAllPersons()
+      .then(result => (this.ServerPersons = result))
+      .catch(() => {});
+
+    this.mongo
+      .getAllInstitutions()
+      .then(result => (this.ServerInstitutions = result))
+      .catch(() => {});
+
+    this.mongo
+      .getAllTags()
+      .then(result => (this.ServerTags = result))
+      .catch(() => {});
+  }
+
+  public getPersons = () => this.ServerPersons;
+  public getInstitutions = () => this.ServerInstitutions;
+  public getTags = () => this.ServerTags;
+
+  // Helper function to fill an array with length of 'length'
+  // with the resulting base validation object of 'func'
+  private arrBase = (func: () => any, valArr: any[]) => {
+    const arr = new Array(valArr.length);
+    for (let i = 0; i < valArr.length; i++) {
+      arr[i] = this.walkSimple(valArr[i], func);
+    }
+    return arr;
+  };
+
+  // Walk simple object types, e.g. person, institution, link, id...
+  public walkSimple = (parsed: any, func: () => any) => {
+    const newSimple = { ...func() };
+    for (const prop in parsed) {
+      if (!parsed.hasOwnProperty(prop)) {
+        continue;
+      }
+
+      const value = parsed[prop];
+      const isArray = Array.isArray(value);
+      const isString = typeof value === 'string';
+      if (!isArray && isString) {
+        newSimple[prop].value = value;
+      } else if (isArray && !isString) {
+        switch (prop) {
+          case 'institution':
+            newSimple[prop].value = this.arrBase(baseInstitution, value);
+            break;
+          default:
+            // Assume string array
+            newSimple[prop].value = value;
+        }
+      } else {
+        switch (prop) {
+          case 'address':
+            newSimple[prop].value = this.walkSimple(value, baseAddress);
+            break;
+          case 'roles':
+            newSimple[prop] = {
+              required: false,
+              value,
+            };
+            break;
+          case 'place':
+          default:
+            console.log('Unknown hit in simple', prop, value, parsed);
+        }
+      }
+    }
+    return newSimple;
+  };
+
+  // Walk digital or physical entities, which are the 2 top level types
+  public walkEntity = (jsonEntity: any, isPhysical = false) => {
+    // Base to be overwritten
+    const newBase = isPhysical
+      ? { ...baseEntity(), ...basePhysical() }
+      : { ...baseEntity(), ...baseDigital() };
+
+    for (const prop in jsonEntity) {
+      if (!jsonEntity.hasOwnProperty(prop)) {
+        continue;
+      }
+
+      const value = jsonEntity[prop];
+      const isArray = Array.isArray(value);
+      const isString = typeof value === 'string';
+      if (!isArray && isString) {
+        newBase[prop].value = value;
+      } else if (isArray && !isString) {
+        const valArr: any[] = value as any[];
+        const len = valArr.length;
+        switch (prop) {
+          case 'externalId':
+            newBase[prop].value = this.arrBase(baseExternalId, valArr);
+            break;
+          case 'externalLink':
+            newBase[prop].value = this.arrBase(baseExternalLink, valArr);
+            break;
+          case 'persons':
+            newBase[prop].value = this.arrBase(basePerson, valArr);
+            break;
+          case 'institutions':
+            newBase[prop].value = this.arrBase(baseInstitution, valArr);
+            break;
+          case 'dimensions':
+            newBase[prop].value = this.arrBase(baseDimension, valArr);
+            break;
+          case 'creation':
+            newBase[prop].value = this.arrBase(baseCreation, valArr);
+            break;
+          case 'phyObjs':
+            newBase[prop].value = new Array(len);
+            for (let i = 0; i < len; i++) {
+              newBase[prop].value[i] = this.walkEntity(valArr[i], true);
+            }
+            break;
+          default:
+            newBase[prop].value = value;
+        }
+      } else {
+        switch (prop) {
+          case 'place':
+            newBase[prop] = {
+              required: false,
+              value: this.walkSimple(value, basePlace),
+            };
+            break;
+          default:
+            console.log('Unknown hit in parsing', prop, value);
+        }
+      }
+    }
+    return newBase;
+  };
+
+  public convertValidationEntityToJSON(entity: any) {
+    const walk = (obj: any) => {
+      const resultObj: any = {};
+      if (typeof obj === 'string') {
+        return obj;
+      }
+
+      for (const prop in obj) {
+        if (!obj.hasOwnProperty(prop)) {
+          continue;
+        }
+        const current = obj[prop];
+        const value = obj[prop].value;
+        const isArray = Array.isArray(value);
+        const isString = typeof value === 'string';
+
+        if (!isArray && isString) {
+          resultObj[prop] = value;
+        } else if (isArray && !isString) {
+          resultObj[prop] = (value as any[]).map(walk);
+        } else {
+          switch (prop) {
+            case 'address':
+            case 'place':
+            case 'roles':
+              resultObj[prop] = walk(value);
+              break;
+            default:
+              console.log(
+                'Unkown hit in conversion',
+                current,
+                prop,
+                obj,
+                obj[prop],
+              );
+          }
+        }
+      }
+      return resultObj;
+    };
+    return walk(entity);
   }
 }
