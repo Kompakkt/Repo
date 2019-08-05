140ceaed79e009925d38ed547ffb9d9bee1c0fde
janwieners <jan@jan-wieners.de>
2019-08-05 11:32:08 +0200

    Show viewer on front page.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from '../../../environments/environment';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+    public viewerUrl: string;
+
+    constructor() {
+        this.viewerUrl = environment.kompakkt_url;
+    }
+
+    ngOnInit() {
+    }
 }
