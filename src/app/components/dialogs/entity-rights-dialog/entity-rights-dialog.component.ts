4d5518dcbaf48a6adf8f060e80e23a1db62d609f
Kai Niebes <kai.niebes@outlook.com>
2019-08-16 00:14:10 +0200

    WIP: EntityRights Dialog

--- src/app/components/dialogs/entity-rights-dialog/entity-rights-dialog.component.ts
+++ src/app/components/dialogs/entity-rights-dialog/entity-rights-dialog.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
+
+import { IEntity, IUserData } from '../../../interfaces';
+import { MongoHandlerService } from '../../../services/mongo-handler.service';
 
 @Component({
   selector: 'app-entity-rights-dialog',
@@ -6,7 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entity-rights-dialog.component.scss'],
 })
 export class EntityRightsDialogComponent implements OnInit {
-  constructor() {}
+  public entity: IEntity | undefined;
 
-  ngOnInit() {}
+  public entityOwners: IUserData[] = [];
+
+  constructor(
+    private dialogRef: MatDialogRef<EntityRightsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) private data: IEntity | undefined,
+    private mongo: MongoHandlerService,
+  ) {}
+
+  ngOnInit() {
+    if (this.data) {
+      this.entity = this.data;
+      this.mongo
+        .findEntityOwners(this.entity._id)
+        .then(result => {
+          if (result.status === 'ok') {
+            this.entityOwners = result.accounts;
+          } else {
+            throw new Error(result.message);
+          }
+        })
+        .catch(e => console.error(e));
+    }
+  }
 }
