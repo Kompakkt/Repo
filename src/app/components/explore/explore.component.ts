a887b30d06e9dc8e46d966fa4dc9311e6b7b2f06
Jan G. Wieners <jan@jan-wieners.de>
2019-08-16 12:58:13 +0200

    Prettify.

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,22 +1,20 @@
-import {Component, OnInit} from '@angular/core';
-import {AccountService} from "../../services/account.service";
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from '../../services/account.service';
 
 @Component({
-    selector: 'app-explore-entities',
-    templateUrl: './explore.component.html',
-    styleUrls: ['./explore.component.scss'],
+  selector: 'app-explore-entities',
+  templateUrl: './explore.component.html',
+  styleUrls: ['./explore.component.scss'],
 })
 export class ExploreComponent implements OnInit {
+  public isAuthenticated;
 
-    public isAuthenticated;
+  constructor(private account: AccountService) {
+    this.isAuthenticated = false;
+    this.account.isUserAuthenticatedObservable.subscribe(
+      state => (this.isAuthenticated = state),
+    );
+  }
 
-    constructor(private account: AccountService) {
-        this.isAuthenticated = false;
-        this.account.isUserAuthenticatedObservable.subscribe(
-            state => (this.isAuthenticated = state),
-        );
-    }
-
-    ngOnInit() {
-    }
+  ngOnInit() {}
 }
