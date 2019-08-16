420b7c3e0d3fa2730b25a36c96c94f1d146d63ac
Jan G. Wieners <jan@jan-wieners.de>
2019-08-16 12:57:59 +0200

    Move create new object link to explore-component.

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,12 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AccountService} from "../../services/account.service";
 
 @Component({
-  selector: 'app-explore-entities',
-  templateUrl: './explore.component.html',
-  styleUrls: ['./explore.component.scss'],
+    selector: 'app-explore-entities',
+    templateUrl: './explore.component.html',
+    styleUrls: ['./explore.component.scss'],
 })
 export class ExploreComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit() {}
+    public isAuthenticated;
+
+    constructor(private account: AccountService) {
+        this.isAuthenticated = false;
+        this.account.isUserAuthenticatedObservable.subscribe(
+            state => (this.isAuthenticated = state),
+        );
+    }
+
+    ngOnInit() {
+    }
 }
