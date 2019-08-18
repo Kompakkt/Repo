8176169b200f8fb0a120f6e0ad96d82811688c7c
Jan G. Wieners <jan@jan-wieners.de>
2019-08-18 23:57:51 +0200

    Prepare object interaction.

--- src/app/components/explore/explore.component.ts
+++ src/app/components/explore/explore.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+
+import { IEntity } from '../../interfaces';
 import { AccountService } from '../../services/account.service';
+import { MongoHandlerService } from '../../services/mongo-handler.service';
 
 @Component({
   selector: 'app-explore-entities',
@@ -7,13 +10,35 @@ import { AccountService } from '../../services/account.service';
   styleUrls: ['./explore.component.scss'],
 })
 export class ExploreComponent implements OnInit {
-  public isAuthenticated;
+  public isAuthenticated: boolean;
+  public entities: IEntity[] = [];
 
-  constructor(private account: AccountService) {
+  public selectedEntity;
+
+  constructor(
+    private account: AccountService,
+    private mongo: MongoHandlerService,
+  ) {
+    this.mongo
+      .getAllEntities()
+      .then(result => {
+        this.entities = result.filter(
+          _entity => _entity.finished && _entity.online,
+        );
+      })
+      .catch(e => console.error(e));
+
+    this.selectedEntity = false;
     this.isAuthenticated = false;
+
     this.account.isUserAuthenticatedObservable.subscribe(
       state => (this.isAuthenticated = state),
     );
+  }
+
+  public select(entity: IEntity) {
+    console.log(entity)
+    this.selectedEntity._id === entity._id ? this.selectedEntity = false : this.selectedEntity = entity;
   }
 
   ngOnInit() {}
