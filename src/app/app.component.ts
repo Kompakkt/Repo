455123cf29a4a6017329b0bb5c8b81b6b0181253
Kai Niebes <kai.niebes@outlook.com>
2019-08-19 12:36:45 +0200

    Router Transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,46 @@ import { environment } from '../environments/environment';
 
 import { TrackingService } from './services/tracking.service';
 
+import {
+  transition,
+  animate,
+  query,
+  style,
+  trigger,
+  group,
+} from '@angular/animations';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('routeTransition', [
+      transition('* <=> *', [
+        query(':enter, :leave', style({ position: 'fixed', width: '100%' }), {
+          optional: true,
+        }),
+        group([
+          query(
+            ':leave',
+            [
+              style({ opacity: 1 }),
+              animate('280ms ease', style({ opacity: 0 })),
+            ],
+            { optional: true },
+          ),
+          query(
+            ':enter',
+            [
+              style({ opacity: 0 }),
+              animate('280ms ease', style({ opacity: 1 })),
+            ],
+            { optional: true },
+          ),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
   title = 'Kompakkt';
