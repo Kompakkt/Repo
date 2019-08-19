b5e04060b259b40407c062944decaa680d49751c
Jan G. Wieners <jan@jan-wieners.de>
2019-08-20 00:22:30 +0200

    Prettify.

--- src/app/components/annotate/annotate.component.ts
+++ src/app/components/annotate/annotate.component.ts
@@ -1,48 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
-import {environment} from '../../../environments/environment';
-import {MongoHandlerService} from '../../services/mongo-handler.service';
+import { environment } from '../../../environments/environment';
+import { MongoHandlerService } from '../../services/mongo-handler.service';
 
 @Component({
-    selector: 'app-annotate',
-    templateUrl: './annotate.component.html',
-    styleUrls: ['./annotate.component.scss'],
+  selector: 'app-annotate',
+  templateUrl: './annotate.component.html',
+  styleUrls: ['./annotate.component.scss'],
 })
 export class AnnotateComponent implements OnInit {
+  public object;
+  public objectID;
+  public viewerUrl: string;
+  public objectReady: boolean;
 
-    public object;
-    public objectID;
-    public viewerUrl: string;
-    public objectReady: boolean;
+  constructor(
+    private route: ActivatedRoute,
+    public mongo: MongoHandlerService,
+  ) {
+    this.viewerUrl = ``;
+    this.objectReady = false;
+  }
 
-    constructor(private route: ActivatedRoute,
-                public mongo: MongoHandlerService) {
-        this.viewerUrl = ``;
-        this.objectReady = false;
-    }
+  ngOnInit() {
+    this.objectID = this.route.snapshot.paramMap.get('id');
+    this.viewerUrl = `${environment.kompakkt_url}?entity=${this.objectID}`;
 
-    ngOnInit() {
-        this.objectID = this.route.snapshot.paramMap.get('id');
-        this.viewerUrl = `${environment.kompakkt_url}?entity=${this.objectID}`;
-
-        this.mongo
-            .getEntity(this.objectID)
-            .then(resultEntity => {
-                if (resultEntity.status !== 'ok') throw new Error('Cannot get object');
-                if (!resultEntity.relatedDigitalEntity) {
-                    throw new Error('Invalid object metadata');
-                }
-                return this.mongo.getEntityMetadata(
-                    resultEntity.relatedDigitalEntity._id,
-                );
-            })
-            .then(result => {
-                this.object = result;
-                this.objectReady = true;
-            })
-            .catch(e => {
-                console.error(e);
-            });
-    }
+    this.mongo
+      .getEntity(this.objectID)
+      .then(resultEntity => {
+        if (resultEntity.status !== 'ok') throw new Error('Cannot get object');
+        if (!resultEntity.relatedDigitalEntity) {
+          throw new Error('Invalid object metadata');
+        }
+        return this.mongo.getEntityMetadata(
+          resultEntity.relatedDigitalEntity._id,
+        );
+      })
+      .then(result => {
+        this.object = result;
+        this.objectReady = true;
+      })
+      .catch(e => {
+        console.error(e);
+      });
+  }
 }
