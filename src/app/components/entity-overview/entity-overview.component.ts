525c07a3c5b9afdc5fc1505ff1f45ac503763fb2
janwieners <jan@jan-wieners.de>
2019-08-30 12:36:29 +0200

    Use own cards on frontpage.

--- src/app/components/entity-overview/entity-overview.component.ts
+++ src/app/components/entity-overview/entity-overview.component.ts
@@ -1,27 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import {IEntity} from '../../interfaces';
-import {MongoHandlerService} from '../../services/mongo-handler.service';
+import { IEntity } from '../../interfaces';
+import { MongoHandlerService } from '../../services/mongo-handler.service';
 
 @Component({
-    selector: 'app-entity-overview',
-    templateUrl: './entity-overview.component.html',
-    styleUrls: ['./entity-overview.component.scss'],
+  selector: 'app-entity-overview',
+  templateUrl: './entity-overview.component.html',
+  styleUrls: ['./entity-overview.component.scss'],
 })
 export class EntityOverviewComponent implements OnInit {
-    public entities: IEntity[] = [];
+  public entities: IEntity[] = [];
 
-    constructor(private mongo: MongoHandlerService) {
-        this.mongo
-            .getAllEntities()
-            .then(result => {
-                this.entities = result.filter(
-                    _entity => _entity.finished && _entity.online,
-                );
-            })
-            .catch(e => console.error(e));
-    }
+  public icons = {
+    audio: 'audiotrack',
+    video: 'movie',
+    image: 'image',
+    model: 'language',
+    collection: 'apps',
+  };
 
-    ngOnInit() {
-    }
+  constructor(private mongo: MongoHandlerService) {
+    this.mongo
+      .getAllEntities()
+      .then(result => {
+        this.entities = result.filter(
+          _entity => _entity.finished && _entity.online,
+        );
+      })
+      .catch(e => console.error(e));
+  }
+
+  ngOnInit() {}
 }
