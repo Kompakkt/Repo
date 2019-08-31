a8e98e50c0371285ba8c28d82afab26dccd17caf
Jan G. Wieners <jan@jan-wieners.de>
2019-09-01 00:43:46 +0200

    Show selected teaser objects on frontpage.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 import { ParticlesConfig } from '../../../assets/particles-config';
 import { environment } from '../../../environments/environment';
+import {MongoHandlerService} from "../../services/mongo-handler.service";
+import {IUserData} from "../../interfaces";
+import {AccountService} from "../../services/account.service";
 
 declare var particlesJS: any;
 
@@ -12,12 +15,47 @@ declare var particlesJS: any;
 })
 export class HomeComponent implements OnInit {
   public viewerUrl: string;
+  public teaserEntities;
+  public isAuthenticated = false;
+  public userData: IUserData | undefined;
 
-  constructor() {
+  public icons = {
+    audio: 'audiotrack',
+    video: 'movie',
+    image: 'image',
+    model: 'language',
+    collection: 'apps',
+  };
+
+  constructor(private account: AccountService,
+              private mongo: MongoHandlerService) {
     this.viewerUrl = `${environment.kompakkt_url}`;
+
+    this.account.isUserAuthenticatedObservable.subscribe(
+        state => (this.isAuthenticated = state),
+    );
+    this.account.userDataObservable.subscribe(newData => {
+      if (!newData) return;
+      this.userData = newData;
+      console.log('Userdata received in ProfilePageComponent', this.userData);
+    });
+  }
+
+  public getCompilationsOfTheMonth() {
+    this.mongo
+        .getCompilation('5d6af3eb72b3dc766b27d748')
+        .then(result => {
+          if (result.status === 'ok') {
+            this.teaserEntities = result.entities;
+          } else {
+            throw new Error(result.message);
+          }
+        })
+        .catch(e => console.error(e));
   }
 
   ngOnInit() {
+    this.getCompilationsOfTheMonth()
     particlesJS('particles', ParticlesConfig, () => {});
   }
 }
