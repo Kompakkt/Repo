a96cda8bb2b7c0d9bb666b181b9f299a69c11283
Kai Niebes <kai.niebes@outlook.com>
2019-09-04 20:35:16 +0200

    Add BrowserSupportService for browser compatibility checking

---
+++ src/app/services/browser-support.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BrowserSupportService {
+  /**
+   * DropEvents in modern browsers use DataTransfer
+   * to list items and make them available as WebKitEntries.
+   * This is experimental, so check for support
+   */
+  private dragDropSupport =
+    (window &&
+      window.DataTransfer &&
+      window.DataTransferItem &&
+      window.DataTransferItemList) !== undefined;
+
+  /**
+   * HTMLInputElements can accept directories as WebKitDirectory
+   * when the property webkitdirectory is available and enabled.
+   * This is experimental, so check for support
+   */
+  public webkitDirSupport =
+    (document.createElement('input') as HTMLInputElement)['webkitdirectory'] !==
+    undefined;
+
+  get isDragDropSupported() {
+    return this.dragDropSupport;
+  }
+
+  get isWebkitDirSupported() {
+    return this.webkitDirSupport;
+  }
+}
