bb8ca08cbda241ebac60525fe5643fa1ecb0da98
Kai Niebes <kai.niebes@outlook.com>
2019-11-20 10:57:00 +0100

    Improve licence and person role display on entity detail page

--- src/app/components/entity-detail/entity-detail.component.ts
+++ src/app/components/entity-detail/entity-detail.component.ts
@@ -28,6 +28,47 @@ export class EntityDetailComponent implements OnInit {
 
   public isAuthenticated = false;
 
+  public roleStrings = {
+    RIGHTS_OWNER: 'Rightsowner',
+    CREATOR: 'Creator',
+    EDITOR: 'Editor',
+    DATA_CREATOR: 'Data Creator',
+    CONTACT_PERSON: 'Contact Person',
+  };
+
+  public Licenses = {
+    BY: {
+      src: 'assets/licence/BY.png',
+      description: 'CC Attribution',
+      link: 'https://creativecommons.org/licenses/by/4.0',
+    },
+    'BY-SA': {
+      src: 'assets/licence/BY-SA.png',
+      description: 'CC Attribution-ShareAlike',
+      link: 'https://creativecommons.org/licenses/by-sa/4.0',
+    },
+    'BY-ND': {
+      src: 'assets/licence/BY-ND.png',
+      description: 'CC Attribution-NoDerivatives',
+      link: 'https://creativecommons.org/licenses/by-nd/4.0',
+    },
+    BYNC: {
+      src: 'assets/licence/BYNC.png',
+      description: 'CC Attribution-NonCommercial',
+      link: 'https://creativecommons.org/licenses/by-nc/4.0',
+    },
+    BYNCSA: {
+      src: 'assets/licence/BYNCSA.png',
+      description: 'CC Attribution-NonCommercial-ShareAlike',
+      link: 'https://creativecommons.org/licenses/by-nc-sa/4.0',
+    },
+    BYNCND: {
+      src: 'assets/licence/BYNCND.png',
+      description: 'CC Attribution-NonCommercial-NoDerivatives',
+      link: 'https://creativecommons.org/licenses/by-nc-nd/4.0',
+    },
+  };
+
   constructor(
     private account: AccountService,
     private route: ActivatedRoute,
@@ -60,17 +101,21 @@ export class EntityDetailComponent implements OnInit {
     entity: IMetaDataDigitalEntity | IMetaDataPhysicalEntity,
     role: string,
   ) =>
-    entity.persons.filter(
-      person =>
-        person.roles[entity._id] && person.roles[entity._id].includes(role),
-    );
+    entity.persons.filter(person => this.getPersonRole(person).includes(role));
+
+  public getPersonRole = (person: IMetaDataPerson) =>
+    this.object ? person.roles[this.object._id] : [];
 
   public getContactRef(person: IMetaDataPerson) {
     if (!this.object) return undefined;
-    return person.contact_references[this.object._id]
-      ? person.contact_references[this.object._id]
-      : Object.keys(person.contact_references).length > 0
-      ? person.contact_references[Object.keys(person.contact_references)[0]]
+    const related = person.contact_references[this.object._id];
+    const latest = Object.values(person.contact_references)
+      .filter(ref => ref.mail !== '')
+      .sort((a, b) => a.creation_date - b.creation_date);
+    return related && related.mail !== ''
+      ? related
+      : latest.length > 0
+      ? latest[0]
       : undefined;
   }
 
