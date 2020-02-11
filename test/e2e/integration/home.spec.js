ec3251f67f574b20dc17e330f580e4e8ca12f271
Jan G. Wieners <jan@jan-wieners.de>
2020-02-11 21:53:08 +0100

    Let test fail to check integration of Cypress into CI environment.

--- test/e2e/integration/home.spec.js
+++ test/e2e/integration/home.spec.js
@@ -6,7 +6,7 @@ describe("Explore", () => {
     it("should open up a login modal on click", () => {
         cy.get('#navbar #btn-login').click();
 
-        cy.get('#username').type('W. Dyer');
+        cy.get('#usernam').type('W. Dyer');
         cy.get('#password').type('m1sk4t0nIkk');
         cy.get('#btn-cancel').click();
     });
