4f68eb1e4eb7f9fc7d3680d3530966f4366ee4dc
janwieners <jan.wieners@uni-koeln.de>
2020-02-18 13:10:43 +0100

    Let test succeed.

--- test/e2e/integration/explore.spec.js
+++ test/e2e/integration/explore.spec.js
@@ -1,6 +1,6 @@
 describe("Explore", () => {
     it("should visit explore page", () => {
-        cy.visit("/explor");
+        cy.visit("/explore");
         cy.location('pathname').should('eq', '/explore')
     });
 });
