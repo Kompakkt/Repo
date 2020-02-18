4253743b960559bd6b7d00f86444037283bbf239
Kai Niebes <kai.niebes@outlook.com>
2020-02-18 22:31:59 +0100

    Environment: Update Viewer URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  kompakkt_url: 'https://kompakkt.uni-koeln.de/viewer/index.html',
+  kompakkt_url: 'https://kompakkt.de/viewer/index.html',
   express_server_url: 'https://kompakkt.uni-koeln.de',
   express_server_port: 8080,
   tracking: true,
