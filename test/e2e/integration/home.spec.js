eb4cf7059e0659d775915de0b08031aa5b65c196
Jan G. Wieners <jan@jan-wieners.de>
2020-03-08 12:42:22 +0100

    Provide e2e test for user login.

--- test/e2e/integration/home.spec.js
+++ test/e2e/integration/home.spec.js
@@ -3,12 +3,4 @@ describe("Explore", () => {
         cy.visit("/home");
         cy.location('pathname').should('eq', '/home')
     });
-
-    it("should open up a login modal on click", () => {
-        cy.get('#navbar #btn-login').click();
-
-        cy.get('#username').type('W. Dyer');
-        cy.get('#password').type('m1sk4t0nIkk');
-        cy.get('#btn-cancel').click();
-    });
 });
---
+++ test/e2e/integration/login.spec.ts
@@ -0,0 +1,41 @@
+describe("Login", () => {
+    it("should visit contact page", () => {
+        cy.visit("/contact");
+        cy.location('pathname').should('eq', '/contact')
+    });
+
+    it("should fail with incorrect user credentials", () => {
+        cy.get('#navbar #login').click();
+
+        cy.get('#username').type('pjeffies');
+        cy.get('#password').type('firewalkwithme');
+        cy.get('#btn-login > .mat-button-wrapper').click();
+
+        cy.get('#login-failed')
+            .should('be.visible')
+            .and('contain', 'Login failed');
+    });
+
+    it("should visit profile page after successful login", () => {
+        cy.get('#username')
+            .clear()
+            .type('pjeffries');
+        cy.get('#password')
+            .clear()
+            .type('firewalkwithme');
+        cy.get('#btn-login > .mat-button-wrapper').click();
+
+        cy.visit("/profile");
+        cy.location('pathname').should('eq', '/profile')
+        cy.get('#profile-page-content h1').should('contain', 'User Profile')
+    });
+
+    it("should logout current user", () => {
+        cy.get('#navbar #logout').click();
+        cy.get('#profile-page-container h2')
+            .should('be.visible')
+            .and('contain', 'No data available for the current user.');
+    });
+
+
+});
