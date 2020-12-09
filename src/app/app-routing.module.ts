6dddde517ed5f39a84f8043db5a1e11f00f683bc
janwieners <mail@jan-wieners.de>
2020-12-09 13:28:03 +0100

    Update Angular.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -119,7 +119,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
