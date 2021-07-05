4314a9f1e0872a8c3a5fa584c4b26f320133dc37
janwieners <mail@jan-wieners.de>
2021-07-05 18:08:12 +0200

    Adjust privacy settings and documentation.

--- src/app/services/tracking.service.ts
+++ src/app/services/tracking.service.ts
@@ -1,15 +1,15 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-import { environment } from '../../environments/environment';
+import { environment } from "../../environments/environment";
 
 declare const window: {
   [key: string]: any;
   prototype: Window;
-  new (): Window;
+  new(): Window;
 };
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root"
 })
 export class TrackingService {
   constructor() {
@@ -17,20 +17,23 @@ export class TrackingService {
   }
 
   public init() {
-    window._paq.push(['trackPageView']);
-    window._paq.push(['enableLinkTracking']);
+    // window._paq.push(["requireCookieConsent"]);
+    window._paq.push(['disableCookies']);
+
+    window._paq.push(["trackPageView"]);
+    window._paq.push(["enableLinkTracking"]);
 
     (() => {
-      window._paq.push(['setTrackerUrl', `${environment.tracking_url}matomo.php`]);
-      window._paq.push(['setSiteId', environment.tracking_id]);
+      window._paq.push(["setTrackerUrl", `${environment.tracking_url}matomo.php`]);
+      window._paq.push(["setSiteId", environment.tracking_id]);
 
-      const newScriptElement = document.createElement('script');
-      newScriptElement.type = 'text/javascript';
+      const newScriptElement = document.createElement("script");
+      newScriptElement.type = "text/javascript";
       newScriptElement.async = true;
       newScriptElement.defer = true;
       newScriptElement.src = `${environment.tracking_url}matomo.js`;
 
-      const firstScript = document.getElementsByTagName('script')[0];
+      const firstScript = document.getElementsByTagName("script")[0];
       if (!firstScript.parentNode) return;
       firstScript.parentNode.insertBefore(newScriptElement, firstScript);
     })();
@@ -38,12 +41,12 @@ export class TrackingService {
 
   public trackPageView(title: string) {
     // Cf. https://developer.matomo.org/guides/spa-tracking
-    window._paq.push(['setCustomUrl', window.location.href]);
-    window._paq.push(['setDocumentTitle', title]);
-    window._paq.push(['deleteCustomVariables', 'page']);
-    window._paq.push(['setGenerationTimeMs', 0]);
-    window._paq.push(['trackPageView']);
+    window._paq.push(["setCustomUrl", window.location.href]);
+    window._paq.push(["setDocumentTitle", title]);
+    window._paq.push(["deleteCustomVariables", "page"]);
+    window._paq.push(["setGenerationTimeMs", 0]);
+    window._paq.push(["trackPageView"]);
 
-    window._paq.push(['enableLinkTracking']);
+    window._paq.push(["enableLinkTracking"]);
   }
 }
