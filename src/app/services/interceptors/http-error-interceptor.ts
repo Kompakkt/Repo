3888fc2df74bdf68f827607c0dcaeadb4a8c8975
Kai Niebes <kai.niebes@outlook.com>
2022-09-07 17:12:33 +0200

    Use Angular HttpInterceptors to better integrate with the HttpClient used by BackendService

---
+++ src/app/services/interceptors/http-error-interceptor.ts
@@ -0,0 +1,28 @@
+import {
+  HttpErrorResponse,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+  HttpResponse,
+  HttpEvent,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+@Injectable()
+export class HttpErrorInterceptor implements HttpInterceptor {
+  constructor() {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
+    return next.handle(req).pipe(
+      catchError(err => {
+        if (err instanceof HttpErrorResponse) {
+          console.warn(`${req.method} request to ${req.url} failed with`, err);
+          return of(new HttpResponse({ body: undefined, status: 200 }));
+        }
+        throw err;
+      }),
+    );
+  }
+}
