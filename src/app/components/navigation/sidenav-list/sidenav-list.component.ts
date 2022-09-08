f86aee2004f79d681c9b9587c6188745d8764522
Kai Niebes <kai.niebes@outlook.com>
2022-09-08 20:34:18 +0200

    Add all navbar options to sidebar and make navbar adjust to smaller screens

--- src/app/components/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/components/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Router } from '@angular/router';
+import { AccountService, DialogHelperService } from '~services';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -8,7 +10,25 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class SidenavListComponent {
   @Output() sidenavClose = new EventEmitter();
 
+  constructor(
+    private account: AccountService,
+    public dialogHelper: DialogHelperService,
+    private router: Router,
+  ) {}
+
+  get isAuthenticated$() {
+    return this.account.isAuthenticated$;
+  }
+
+  get isAdmin$() {
+    return this.account.isAdmin$;
+  }
+
   public onSidenavClose = () => {
     this.sidenavClose.emit();
   };
+
+  public logout() {
+    this.account.logout().then(() => this.router.navigate(['/']));
+  }
 }
