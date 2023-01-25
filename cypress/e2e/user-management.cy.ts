9a9e342f67bfbeda5c4cf8b0212f1d881dac2fcd
Kai Niebes <kai.niebes@outlook.com>
2023-01-25 17:50:37 +0100

    Organize user-management tests into categories and add profile page test

--- cypress/e2e/user-management.cy.ts
+++ cypress/e2e/user-management.cy.ts
@@ -19,71 +19,101 @@ describe('User Management and Authentication', () => {
     cy.visit(baseUrl + '/home');
   });
 
-  it('Should be able to register a new account', () => {
-    cy.get('#navbar a#register').click();
-    cy.wait(1000);
+  describe('Registration', () => {
+    it('Should be able to register a new account', () => {
+      cy.get('#navbar a#register').click();
+      cy.wait(1000);
 
-    cy.get('app-register-dialog input[name=mail]').type(email);
-    cy.get('app-register-dialog input[name=prename]').type(prename);
-    cy.get('app-register-dialog input[name=surname]').type(surname);
-    cy.get('app-register-dialog input[name=username]').type(username);
-    cy.get('app-register-dialog input[name=password').type(password);
-    cy.get('app-register-dialog input[name=passwordRepeat').type(password);
-    cy.get('app-register-dialog button#create-account-button').click();
+      cy.get('app-register-dialog input[name=mail]').type(email);
+      cy.get('app-register-dialog input[name=prename]').type(prename);
+      cy.get('app-register-dialog input[name=surname]').type(surname);
+      cy.get('app-register-dialog input[name=username]').type(username);
+      cy.get('app-register-dialog input[name=password').type(password);
+      cy.get('app-register-dialog input[name=passwordRepeat').type(password);
+      cy.get('app-register-dialog button#create-account-button').click();
 
-    cy.wait('@register').then(({ response }) => {
-      expect(response?.statusCode).to.equal(201);
-      expect(response?.body['status']).to.equal('OK');
+      cy.wait('@register').then(({ response }) => {
+        expect(response?.statusCode).to.equal(201);
+        expect(response?.body['status']).to.equal('OK');
+      });
+    });
+
+    it('Should display error message when trying to register with existing email', () => {
+      cy.get('#navbar a#register').click();
+      cy.wait(1000);
+
+      const prename = faker.name.firstName();
+      const surname = faker.name.lastName();
+      const password = faker.internet.password();
+
+      cy.get('app-register-dialog input[name=mail]').type(email);
+      cy.get('app-register-dialog input[name=prename]').type(prename);
+      cy.get('app-register-dialog input[name=surname]').type(surname);
+      cy.get('app-register-dialog input[name=username]').type(username);
+      cy.get('app-register-dialog input[name=password').type(password);
+      cy.get('app-register-dialog input[name=passwordRepeat').type(password);
+      cy.get('app-register-dialog button#create-account-button').click();
+
+      cy.wait('@register').then(({ response }) => {
+        expect(response?.statusCode).to.equal(409);
+        cy.get('app-register-dialog div.errors').should('exist');
+      });
     });
   });
 
-  it('Should display error message when trying to register with existing email', () => {
-    cy.get('#navbar a#register').click();
-    cy.wait(1000);
+  describe('Logging in', () => {
+    it('Should display error message when trying to login with incorrect credentials', () => {
+      cy.get('#navbar a#login').click();
+      cy.wait(1000);
 
-    const prename = faker.name.firstName();
-    const surname = faker.name.lastName();
-    const password = faker.internet.password();
+      cy.get('app-auth-dialog input[name="username"]').type('fake@example.com');
+      cy.get('app-auth-dialog input[name="password"]').type('fakepassword');
+      cy.get('app-auth-dialog button#btn-login').click();
 
-    cy.get('app-register-dialog input[name=mail]').type(email);
-    cy.get('app-register-dialog input[name=prename]').type(prename);
-    cy.get('app-register-dialog input[name=surname]').type(surname);
-    cy.get('app-register-dialog input[name=username]').type(username);
-    cy.get('app-register-dialog input[name=password').type(password);
-    cy.get('app-register-dialog input[name=passwordRepeat').type(password);
-    cy.get('app-register-dialog button#create-account-button').click();
+      cy.wait('@login').then(({ response }) => {
+        expect(response?.statusCode).to.equal(401);
+        cy.get('app-auth-dialog div.errors').should('exist');
+      });
+    });
 
-    cy.wait('@register').then(({ response }) => {
-      expect(response?.statusCode).to.equal(409);
-      cy.get('app-register-dialog div.errors').should('exist');
+    it('Should be able to login with correct credentials', () => {
+      cy.get('#navbar a#login').click();
+      cy.wait(1000);
+
+      cy.get('app-auth-dialog input[name="username"]').type(username);
+      cy.get('app-auth-dialog input[name="password"]').type(password);
+      cy.get('app-auth-dialog button#btn-login').click();
+
+      cy.wait('@login').then(({ response }) => {
+        expect(response?.statusCode).to.equal(200);
+        expect(response?.body['_id']).not.to.be.empty;
+      });
     });
   });
 
-  it('Should be able to login with correct credentials', () => {
-    cy.get('#navbar a#login').click();
-    cy.wait(1000);
+  describe('Profile', () => {
+    before(() => {
+      cy.intercept('POST', 'user-management/login').as('login');
 
-    cy.get('app-auth-dialog input[name="username"]').type(username);
-    cy.get('app-auth-dialog input[name="password"]').type(password);
-    cy.get('app-auth-dialog button#btn-login').click();
+      const { baseUrl } = Cypress.config();
+      cy.visit(baseUrl + '/home');
 
-    cy.wait('@login').then(({ response }) => {
-      expect(response?.statusCode).to.equal(200);
-      expect(response?.body['_id']).not.to.be.empty;
+      cy.get('#navbar a#login').click();
+      cy.wait(1000);
+
+      cy.get('app-auth-dialog input[name="username"]').type(username);
+      cy.get('app-auth-dialog input[name="password"]').type(password);
+      cy.get('app-auth-dialog button#btn-login').click();
+
+      cy.wait('@login');
     });
-  });
 
-  it('Should display error message when trying to login with incorrect credentials', () => {
-    cy.get('#navbar a#login').click();
-    cy.wait(1000);
+    it('Visit the users profile', () => {
+      cy.get('#navbar a[href="/profile"]').click();
+      cy.wait(1000);
 
-    cy.get('app-auth-dialog input[name="username"]').type('fake@example.com');
-    cy.get('app-auth-dialog input[name="password"]').type('fakepassword');
-    cy.get('app-auth-dialog button#btn-login').click();
-
-    cy.wait('@login').then(({ response }) => {
-      expect(response?.statusCode).to.equal(401);
-      cy.get('app-auth-dialog div.errors').should('exist');
+      cy.location().url().should('contain', 'profile');
+      cy.get('div#profile-page-content').should('exist');
     });
   });
 });
