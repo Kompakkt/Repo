fcb48d747eaadb48a233f3eddfab2a5dca92b24a
Senya Bär <senya.baer@gmail.com>
2024-10-14 17:51:29 +0200

    work on the institutional page. layout design.

--- src/app/pages/institutional-page/institutional-page.component.ts
+++ src/app/pages/institutional-page/institutional-page.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 import { TranslatePipe } from 'src/app/pipes';
 import { TranslatePipe as TranslatePipe_1 } from '../../pipes/translate.pipe';
+import { ActionbarComponent, GridElementComponent } from 'src/app/components';
+import { MatButton } from '@angular/material/button';
+import { MatMenu } from '@angular/material/menu';
+import { MatIcon } from '@angular/material/icon';
+import { AsyncPipe } from '@angular/common';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
     selector: 'app-institutional-page',
     templateUrl: './institutional-page.component.html',
     styleUrls: ['./institutional-page.component.scss'],
     standalone: true,
-    imports: [TranslatePipe_1],
+    imports: [
+      TranslatePipe_1, 
+      ActionbarComponent, 
+      GridElementComponent,
+      MatPaginator,
+      MatMenu,
+      MatButton,
+      MatIcon,
+      AsyncPipe,
+      MatToolbar,
+    ],
   })
 
 export class InstitutionalPageComponent implements OnInit {
