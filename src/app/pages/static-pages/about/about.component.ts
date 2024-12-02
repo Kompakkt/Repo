7dbb74feb446ffff864e46b88e6cc3893f478eca
Senya Bär <senya.baer@gmail.com>
2024-12-02 14:13:40 +0100

    new Consortium/About page

--- src/app/pages/static-pages/about/about.component.ts
+++ src/app/pages/static-pages/about/about.component.ts
@@ -1,16 +1,81 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { Meta, Title } from '@angular/platform-browser';
 import { TranslatePipe } from 'src/app/pipes';
-import { TranslatePipe as TranslatePipe_1 } from '../../../pipes/translate.pipe';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
   standalone: true,
-  imports: [TranslatePipe_1],
+  imports: [TranslatePipe,
+    MatIcon,
+  ],
 })
 export class AboutComponent implements OnInit {
+  teams = [
+    {
+      name: 'Department of Digital Humanities',
+      logoUrl: 'assets/images/IDH-logo.png',
+      projectCoordinators: [
+        {
+          name: 'Prof. Dr. Øyvind Eide',
+          url: 'https://gitlab.com/oyvindeide',
+        },
+      ],
+      developers: [
+        {
+          name: 'Nadjim Noori',
+          url: 'https://github.com/NadNo12',
+        },
+        {
+          name: 'Senya Bär',
+          url: 'https://github.com/Grizzly127',
+        },
+        {
+          name: 'Vera Marlieske',
+          url: 'https://github.com/vmalieske',
+        },
+      ],
+    },
+    {
+      name: 'Technische Informationsbibliothek (TIB)',
+      logoUrl: 'assets/images/TIB_Logo.png',
+      projectCoordinators: [
+        {
+          name: 'Prof. Dr. Ina Blümel',
+          url: 'https://gitlab.com/inablu',
+        },
+        {
+          name: 'Dr. Lozana Rossenova',
+          url: 'https://github.com/lozanaross',
+        },
+        {
+          name: 'Zoe Schubert',
+          url: 'https://github.com/ZetOE',
+        },
+      ],
+      developers: [
+        {
+          name: 'Kai Niebes',
+          url: 'https://github.com/HeyItsBATMAN',
+        },
+        {
+          name: 'Kolja Bailly',
+          url: 'https://gitlab.com/BaillyK',
+        },
+        {
+          name: 'Lukas Günther',
+          url: 'https://github.com/luguenth',
+        },
+        {
+          name: 'Lucia Sohmen',
+          url: 'https://gitlab.com/sohmenl',
+        },
+      ],
+    },
+  ];
+
   constructor(
     private translatePipe: TranslatePipe,
     private titleService: Title,
@@ -18,10 +83,10 @@ export class AboutComponent implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.titleService.setTitle('Kompakkt – ' + this.translatePipe.transform('Contact'));
+    this.titleService.setTitle('Kompakkt – ' + this.translatePipe.transform('About'));
     this.metaService.updateTag({
       name: 'description',
-      content: 'Kompakkt contact informations.',
+      content: 'About Kompakkt',
     });
   }
 }
