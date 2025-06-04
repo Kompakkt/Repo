e2d87405e47f1a00ac9c05028591b0ee2d1f52fd
Senya Bär <44752496+Grizzly127@users.noreply.github.com>
2025-06-04 17:25:45 +0200

    outsourcing collections and groups into an own component (#95)

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -46,6 +46,8 @@ import {
 import { ActionbarComponent } from '../../components/actionbar/actionbar.component';
 import { GridElementComponent } from '../../components/grid-element/grid-element.component';
 import { ProfilePageHelpComponent } from './profile-page-help.component';
+import { CollectionsComponent } from 'src/app/components/profile/collections/collections.component';
+import { GroupsComponent } from 'src/app/components/profile/groups/groups.component';
 import DeepClone from 'rfdc';
 const deepClone = DeepClone({ circles: true });
 
@@ -77,6 +79,8 @@ const deepClone = DeepClone({ circles: true });
     FormsModule,
     AsyncPipe,
     TranslatePipe,
+    GroupsComponent,
+    CollectionsComponent,
   ],
 })
 export class ProfilePageComponent implements OnInit {
@@ -88,12 +92,6 @@ export class ProfilePageComponent implements OnInit {
     restricted: false,
     unfinished: false,
   };
-
-  public showPartakingGroups = false;
-  public showPartakingCompilations = false;
-
-  private __partakingGroups: IGroup[] = [];
-  private __partakingCompilations: ICompilation[] = [];
 
   public icons = {
     audio: 'audiotrack',
@@ -126,16 +124,6 @@ export class ProfilePageComponent implements OnInit {
     this.account.user$.subscribe(newData => {
       this.userData = newData;
       if (!this.userData) return;
-      this.backend
-        .findUserInGroups()
-        .then(groups => (this.__partakingGroups = groups))
-        .catch(e => console.error(e));
-
-      this.backend
-        .findUserInCompilations()
-        .then(compilations => (this.__partakingCompilations = compilations))
-        .catch(e => console.error(e));
-      this.updateFilter();
     });
   }
 
@@ -272,132 +260,6 @@ export class ProfilePageComponent implements OnInit {
             _e => _e._id !== entity._id,
           );
           this.updateFilter();
-        }
-      })
-      .catch(e => console.error(e));
-  }
-
-  // Groups
-  get userGroups(): IGroup[] {
-    return this.userData?.data?.group?.filter(isGroup) ?? [];
-  }
-
-  get partakingGroups(): IGroup[] {
-    return this.__partakingGroups;
-  }
-
-  public openGroupCreation(group?: IGroup) {
-    const dialogRef = this.dialog.open(AddGroupWizardComponent, {
-      data: group ? deepClone(group) : undefined,
-      disableClose: true,
-    });
-    dialogRef
-      .afterClosed()
-      .toPromise()
-      .then((result: undefined | IGroup) => {
-        if (!result) return;
-        if (!this.userData) return;
-        // Add new group to list
-        const index = this.userData.data.group?.findIndex(g => areDocumentsEqual(g, result)) ?? -1;
-        if (index !== -1) {
-          this.userData.data.group![index] = result;
-        } else {
-          this.userData.data.group = this.userData.data.group
-            ? [...this.userData.data.group, result]
-            : [result];
-        }
-      });
-  }
-
-  public openMemberList(group: IGroup) {
-    this.dialog.open(GroupMemberDialogComponent, {
-      data: group,
-    });
-  }
-
-  public async removeGroupDialog(group: IGroup) {
-    const loginData = await this.helper.confirmWithAuth(
-      `Do you really want to delete ${group.name}?`,
-      `Validate login before deleting ${group.name}`,
-    );
-    if (!loginData) return;
-    const { username, password } = loginData;
-
-    this.backend
-      .deleteRequest(group._id, 'group', username, password)
-      .then(result => {
-        if (this.userData?.data?.group) {
-          this.userData.data.group = (this.userData.data.group as IGroup[]).filter(
-            _g => _g._id !== group._id,
-          );
-        }
-      })
-      .catch(e => console.error(e));
-  }
-
-  public leaveGroupDialog(group: IGroup) {
-    const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      data: `Do you really want to leave ${group.name}?`,
-    });
-    dialogRef
-      .afterClosed()
-      .toPromise()
-      .then(result => {
-        if (result) {
-          // TODO: leave
-          console.log('Leave', group);
-        }
-      });
-  }
-
-  // Compilations
-  get userCompilations(): ICompilation[] {
-    return this.userData?.data?.compilation?.filter(isCompilation) ?? [];
-  }
-
-  get partakingCompilations(): ICompilation[] {
-    return this.__partakingCompilations;
-  }
-
-  public openCompilationCreation(compilation?: ICompilation) {
-    const dialogRef = this.dialog.open(AddCompilationWizardComponent, {
-      data: compilation,
-      disableClose: true,
-    });
-    dialogRef
-      .afterClosed()
-      .toPromise()
-      .then((result: undefined | ICompilation) => {
-        if (result && this.userData && this.userData.data.compilation) {
-          if (compilation) {
-            const index = (this.userData.data.compilation as ICompilation[]).findIndex(
-              comp => comp._id === result._id,
-            );
-            if (index === -1) return;
-            this.userData.data.compilation.splice(index, 1, result);
-          } else {
-            (this.userData.data.compilation as ICompilation[]).push(result);
-          }
-        }
-      });
-  }
-
-  public async removeCompilationDialog(compilation: ICompilation) {
-    const loginData = await this.helper.confirmWithAuth(
-      `Do you really want to delete ${compilation.name}?`,
-      `Validate login before deleting: ${compilation.name}`,
-    );
-    if (!loginData) return;
-    const { username, password } = loginData;
-
-    // Delete
-    this.backend
-      .deleteRequest(compilation._id, 'compilation', username, password)
-      .then(result => {
-        if (this.userData?.data?.compilation) {
-          this.userData.data.compilation = (
-            this.userData.data.compilation as ICompilation[]
-          ).filter(comp => comp._id !== compilation._id);
         }
       })
       .catch(e => console.error(e));
