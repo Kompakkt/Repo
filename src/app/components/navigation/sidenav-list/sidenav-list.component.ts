8a065307dff2af8b3f3509926116d993094bee7b
Kai Niebes <kai.niebes@outlook.com>
2025-11-06 22:56:19 +0100

    Cleanup: Remove queries.scss in favor of breakpoints.scss, and make sure that navbar breaks to mobile layout early enough

--- src/app/components/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/components/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,36 +1,38 @@
-import {
-  Component,
-  EventEmitter,
-  inject,
-  input,
-  InputSignal,
-  output,
-  Output,
-  OutputEmitterRef,
-  signal,
-} from '@angular/core';
-import { TranslateService } from '../../../services/translate.service';
-import { TranslatePipe } from '../../../pipes/translate.pipe';
-import { MatDivider, MatDividerModule } from '@angular/material/divider';
-import { MatIcon, MatIconModule } from '@angular/material/icon';
-import { RouterLink, RouterModule } from '@angular/router';
-import { MatNavList, MatListItem, MatListModule } from '@angular/material/list';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject, input, output, signal } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { RouterModule } from '@angular/router';
+import { ObservableValuePipe } from 'src/app/pipes/observable-value';
+import { AccountService, DialogHelperService } from 'src/app/services';
 import { SidenavComponent, SidenavService } from 'src/app/services/sidenav.service';
-import { DialogHelperService } from 'src/app/services';
+import { TranslatePipe } from '../../../pipes/translate.pipe';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss'],
-  imports: [MatListModule, RouterModule, MatIconModule, MatDividerModule, TranslatePipe],
+  imports: [
+    MatListModule,
+    RouterModule,
+    MatIconModule,
+    MatDividerModule,
+    TranslatePipe,
+    AsyncPipe,
+    ObservableValuePipe,
+    MatProgressBarModule,
+  ],
 })
 export class SidenavListComponent implements SidenavComponent {
-  title = signal('Kompakkt');
+  title = signal('Navigation');
   dataInput = input<unknown>();
   resultChanged = output<unknown>();
 
   sidenavService = inject(SidenavService);
   dialogHelper = inject(DialogHelperService);
+  account = inject(AccountService);
 
   close() {
     this.sidenavService.close();
