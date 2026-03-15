06e898eabba66454debf8bb9044c917d863d4c8e
Vera Malieske <vera.malieske@uni-koeln.de>
2026-03-15 18:47:48 +0100

    Change double-click to single-click to navigate on explore page and profile-page  (#184)
    * Change double-click to single-click to navigate on explore page
    
    * Change navigate on single-click on profile page

--- src/app/components/grid-element/grid-element.component.ts
+++ src/app/components/grid-element/grid-element.component.ts
@@ -11,6 +11,7 @@ import { TitleContainerComponent } from './title-container/title-container.compo
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { TranslatePipe } from 'src/app/pipes';
 import { Router } from '@angular/router';
+import { SelectionService } from 'src/app/services/selection.service';
 
 @Component({
   selector: 'app-grid-element',
@@ -28,7 +29,7 @@ import { Router } from '@angular/router';
   ],
   host: {
     '[class.cursor-pointer]': 'enableNavigationOnClick()',
-    '(dblclick)': 'onDblclick()',
+    '(click)': 'navigateOnClick()',
   },
 })
 export class GridElementComponent {
@@ -36,6 +37,7 @@ export class GridElementComponent {
   disableTypeInfo = input(false);
   quickAddToCollectionMenu = input<MatMenu>();
   #router = inject(Router);
+  #selectionservice = inject(SelectionService);
 
   @Output()
   updateSelectedObject = new EventEmitter<string>();
@@ -71,7 +73,7 @@ export class GridElementComponent {
     this.updateSelectedObject.emit(id.toString());
   }
 
-  public onDblclick() {
+  public navigateOnClick() {
     if (this.enableNavigationOnClick()) {
       const element = this.element();
       if (isEntity(element)) this.#router.navigate(['/entity', element._id]);
